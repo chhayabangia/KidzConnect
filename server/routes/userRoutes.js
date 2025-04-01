745cc195b37820ab3adef3b36cda3c5abedc3fe0
LilmissKryss <161416625+LilmissKryss@users.noreply.github.com>
2025-04-01 16:43:14 -0500

    Chore: Backend setup
    - Initialize Express Server
    - Set up an Express.js server to handle API requests
    - Configure MongoDB Connection

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Route to create a new user
+router.post('/register', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = router;
