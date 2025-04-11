55e4cd117637e8ba5b215ce2d03e87e25d4ad459
LilmissKryss <161416625+LilmissKryss@users.noreply.github.com>
2025-04-10 21:39:40 -0500

    fix: Resolving Render deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
@@ -35,4 +36,10 @@ export default defineConfig({
       },
     },
   },
-});
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+}));
